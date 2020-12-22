d7129f4c1974e2aed8948b0c7c3813dc474e27d7
Vegard Hansen Stenberg <vegard.koder@gmail.com>
2020-12-22 15:42:47 +0100

    the thing seems to bbe working now, going to add more user experience stuff next

--- main.py
+++ main.py
@@ -7,22 +7,26 @@ path = 'test_data/*'
 classes = []
 classes.extend(glob(path))
 
-def create_test_train_folders(p):
+def create_test_train_folders(p): #making test and train folder for given path, meant to be used in subdir's
     for f in ['train', 'test']:
         try:
             os.mkdir(os.path.join(p, f))
         except:
             print("Couldn't create folder:", f, "at:", f)
 
-def move_images(p, train_pros=0.5):
+def move_images(p, test_pros=0.5):
     images = []
-    images.extend(glob(os.path.join(p, '*.jpg')))
+    images.extend(glob(os.path.join(p, '*.jpg'))) #accessing all the images
 
-    train_img_amt = int(len(images) * train_pros)
+    test_img_amt = int(len(images) * test_pros) #calculating the amount of test images based on test_pros
 
-    for i in range(train_img_amt):
-        shutil.move(images[i], os.path.join(c, 'train'))
+    for i in range(test_img_amt): #moving images into the test folder
+        shutil.move(images[i], os.path.join(c, 'test'))
+        images.pop(i) #removing the image from the list, since we do not need it
 
-for c in classes:
+    for img in images: #moving the rest of the images to train folder
+        shutil.move(img, os.path.join(c, 'train'))
+
+for c in classes: #goes through all the folders/classes
     create_test_train_folders(c)
     move_images(c)
